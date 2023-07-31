8dc76500f03700a23b4ceb18cc8ecf71b5084d2c
Nijat Muradov <live4ever4@gmail.com>
2023-07-31 12:08:35 +0400

    Add project files.

---
+++ WeaponMenu/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WeaponMenu
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
